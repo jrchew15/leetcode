0c99ac69d2f355971d3775f0788db3c9087b154c
Jason Chew <jrchew15@gmail.com>
2023-02-06 22:58:54 -0800

    commented fruit basket

--- fruitBasket.js
+++ fruitBasket.js
@@ -9,14 +9,20 @@ var totalFruit = function(fruits) {
     // most recent switch at j
     for (let i=0; i < fruits.length; i++) {
         if (fruits[i] !== current) {
+            // if next fruit is neither of the two we are currently tracking...
             if (prev !== fruits[i]) {
+                // check if current window is greater than max
                 if (max < i - k) {
                     max = i - k
                 }
+                // the left side of our window is updated to the most recent switch
+                // so that on next iteration, our window will only contain 2 different fruits
                 k = j
             }
+            // regardless of above, we need to update which fruit was seen most recently
             prev = current;
             current = fruits[i];
+            // and we need to update most recent switch pointer
             j = i
         }
     }
