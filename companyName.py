864bcef6e3e469871cb08d53b5924c378e62faa9
Jason Chew <jrchew15@gmail.com>
2023-02-09 16:18:52 -0800

    companyNames

---
+++ companyName.py
@@ -0,0 +1,17 @@
+def distinctNames(ideas) -> int:
+        dct = {}
+        for idea in ideas:
+            if idea[0] in dct:
+                dct[idea[0]].add(idea[1:])
+            else:
+                dct[idea[0]] = set()
+                dct[idea[0]].add(idea[1:])
+
+        firsts = list(dct.keys())
+        count = 0
+
+        for i in range(len(firsts)):
+            for j in range(i+1,len(firsts)):
+                diff = len(dct[firsts[i]].intersection(dct[firsts[j]]))
+                count += 2 * (len(dct[firsts[i]])-diff) * (len(dct[firsts[j]])-diff)
+        return count
