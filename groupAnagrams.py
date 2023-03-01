4d9c9707fda17065b410d1565721c3d6779ffbbf
Jason Chew <jrchew15@gmail.com>
2023-03-01 09:27:53 -0800

    with defaultdict

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -1,14 +1,12 @@
 def groupAnagrams(strs):
-    dct = {}
+    from collections import defaultdict
+    dct = defaultdict(lambda: [])
 
     for string in strs:
         chars=list(string)
         chars.sort()
         key = ''.join(chars)
-        if key in dct:
-            dct[key].append(string)
-        else:
-            dct[key] = [string]
+        dct[key].append(string)
     return list(dct.values())
 
 strs1 = ["eat","tea","tan","ate","nat","bat"]
