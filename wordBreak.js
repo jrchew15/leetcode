873067742a81cc4ddc37f3635050a4101edb7763
Jason Chew <jrchew15@gmail.com>
2023-03-13 10:46:48 -0700

    depth first wordbreak

--- wordBreak.js
+++ wordBreak.js
@@ -17,24 +17,37 @@ function makeTrie(dict) {
 
 function wordBreak(s, wordDict) {
     const trie = makeTrie(wordDict);
-    let node = trie;
-    let q = [node];
+    // let node = trie;
+    // let q = [node];
 
-    for (let i = 0; i < s.length; i++) {
-        let char = s[i];
-        let temp = [];
-        for (let curr of q) {
-            if (curr[char]) {
-                temp.push(curr[char])
-                if (curr[char].word) temp.push(trie)
-            }
+    function _wordBreak(idx) {
+        if (idx >= s.length) return true;
+        let node = trie;
+        for (let i = idx; i < s.length; i++) {
+            node = node[s[i]]
+            if (!node) return false;
+            if (node.word && _wordBreak(i + 1)) return true;
         }
-        q = temp
+        return !!node.word
     }
-    return q.length > 0 && q.some(x => x.word)
+    return _wordBreak(0)
+
+    // for (let i = 0; i < s.length; i++) {
+    //     let char = s[i];
+    //     let temp = [];
+    //     for (let curr of q) {
+    //         if (curr[char]) {
+    //             temp.push(curr[char])
+    //             if (curr[char].word) temp.push(trie)
+    //         }
+    //     }
+    //     q = temp
+    // }
+    // return q.length > 0 && q.some(x => x.word)
 };
 
 let x = makeTrie(['leet', 'code', 'leaf', 'leetcode'])
 console.log(x.l.e)
-console.log(wordBreak('leetcode', ['leet', 'code', 'leaf']))
+console.log(wordBreak('leetcode', ['leet', 'cod', 'leaf']))
 console.log(wordBreak('catdog', ['cat', 'cats', 'dog', 'doggie', 'mouse']))
+console.log(wordBreak('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',['a','aa','aaaaaaaaaaa']))
