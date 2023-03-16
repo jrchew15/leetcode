32ce2aa284425be689e7c6418cd0e08e4d554b96
Jason Chew <jrchew15@gmail.com>
2023-03-15 17:48:05 -0700

    incomplete bt construction from in/post order

---
+++ reconstructBinaryTree.js
@@ -0,0 +1,25 @@
+class TreeNode {
+    constructor(val = 0, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+var buildTree = function (inorder, postorder) {
+    if (inorder.length === 1) return new TreeNode(inorder[0])
+    if (inorder.length === 0) return null
+
+    let i = 0;
+    let j = 0;
+    let curr = null;
+    while (i < inorder.length) {
+        j = postorder.indexOf(inorder[i]) + 1;
+        console.log(i, j)
+        curr = new TreeNode(inorder[i], curr, buildTree(inorder.slice(i, j), postorder.slice(i, j)));
+        i = j
+    }
+    return curr
+};
+
+console.log(buildTree([9, 3, 15, 20, 7], [9, 15, 7, 20, 3]))
